6190113eb25af629167dc97b9291fa3560f0ca46
Nizomiddinkhoja <nizomiddinkhoja@gmail.com>
2020-12-14 16:32:03 +0500

    put buld

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+
+  rootUrl: 'https://api.themoviedb.org/3/',
+  imgRootUrl: 'https://image.tmdb.org/t/p/w500',
+  imgUrl: 'https://image.tmdb.org/t/p/w500',
+
 };
